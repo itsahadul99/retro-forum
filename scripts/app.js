f2ecbab6eb0d43c63611bb927c9609a7762aa6c5
itsahadul99 <itsahadul999@gmail.com>
2024-03-03 00:29:36 +0600

    fetch latest post data and show on the ui

--- scripts/app.js
+++ scripts/app.js
@@ -1,12 +1,14 @@
 
 const postContainer = document.getElementById('post-container');
+const showTitle = document.getElementById('show-title');
+const showTitleContaier = document.getElementById('show-title-container')
 const loadAllPost = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
     const data = await res.json();
     // console.log(data.posts);
     const posts = data.posts;
     posts.forEach(post => {
-        console.log(post);
+        // console.log(post);
         const postDiv = document.createElement('div');
         postDiv.className = ``;
         postDiv.innerHTML = `
@@ -47,11 +49,60 @@ const loadAllPost = async () => {
         `
         postContainer.appendChild(postDiv);
     });
+    // const emailButtons = document.querySelectorAll('.email-btn');
+    //     for(const emailBtn of emailButtons){
+    //         emailBtn.addEventListener('click', (posts) =>{
+    //             console.log(posts);
+    //             const div = document.createElement('div');
+    //             div.className = `bg-white flex justify-between items-center rounded-xl p-5 `;
+
+    //             // console.log(post);
+    //             showTitle.innerText = ``;
+    //             showTitleContaier.appendChild(div);
+    //         })
+    //     }
+}
+const latestPostContainer = document.getElementById('latest-post-container');
+const loadLatestPost = async () => {
+    const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/latest-posts`);
+    const data = await res.json();
+    // console.log(data);
+    data.forEach((post) => {
+        console.log(post);
+        const div = document.createElement('div');
+        div.className = `border-2 p-5 lg:p-10 rounded-2xl space-y-5`;
+        div.innerHTML = `
+                    <div class="bg-[#12132D15] w-full min-h-32">
+                    <img src="${post.cover_image} class ="rounded-xl"/>
+                    </div>
+                    <div>
+                        <div class="flex justify-start items-center gap-4">
+                            <img src="./images/date.png" alt="">
+                            <p class="text-[#12132D99] text-sm">${post.author.posted_date?post.author.posted_date:"No publish date"}</p>
+                        </div>
+                        <div>
+                            <h1 class="text-black font-bold text-xl">${post.title}</h1>
+                            <p class="text-[#12132D99] text-sm">${post.description}</p>
+                        </div>
+                        <div class="flex items-start gap-5 lg:gap-10 mt-5">
+                            <div>
+                                <img class ="w-16 rounded-full" src="${post.profile_image}" alt="">
+                            </div>
+                            <div>
+                                <h1 class="font-bold text-[#12132D]">${post.author.name}</h1>
+                                <p class="text-[#12132D99] text-sm">${post.author.designation?post.author.designation:"unknown"}</p>
+                            </div>
+                        </div>
+                    </div>
+        
+        `
+        latestPostContainer.appendChild(div);
+    })
 }
 
 
 
 
 
-
 loadAllPost()
+loadLatestPost();
